c1c44d45074384ac81c3c36932f4b4800eb4618a
etozhenerk <etozhenerk@yandex.ru>
2020-12-27 15:45:50 +0300

    Комментарии ко всем строкам кода

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-const filterByType = (type, ...values) => values.filter(value => typeof value === type),
+const filterByType = (type, ...values) => values.filter(value => typeof value === type),//объявление функции, которая принимает 
 
 	hideAllResponseBlocks = () => {
 		const responseBlocksArray = Array.from(document.querySelectorAll('div.dialog__response-block'));
+		console.log(responseBlocksArray);
 		responseBlocksArray.forEach(block => block.style.display = 'none');
 	},
 
@@ -31,19 +32,19 @@ const filterByType = (type, ...values) => values.filter(value => typeof value ==
 		}
 	};
 
-const filterButton = document.querySelector('#filter-btn');
+const filterButton = document.querySelector('#filter-btn'); // получение кнопки "Фильтровать"
 
-filterButton.addEventListener('click', e => {
-	const typeInput = document.querySelector('#type');
-	const dataInput = document.querySelector('#data');
+filterButton.addEventListener('click', e => { // прослушевание события клик на кнопке "Фильтровать"
+	const typeInput = document.querySelector('#type');// получение инпута с типом данных по id
+	const dataInput = document.querySelector('#data');// получение инпута с данными по id
 
-	if (dataInput.value === '') {
-		dataInput.setCustomValidity('Поле не должно быть пустым!');
-		showNoResults();
-	} else {
-		dataInput.setCustomValidity('');
-		e.preventDefault();
-		tryFilterByType(typeInput.value.trim(), dataInput.value.trim());
+	if (dataInput.value === '') { // проверка на пустую строку в инпуте с данными
+		dataInput.setCustomValidity('Поле не должно быть пустым!'); // метод устанавливает сообщение о том, что поле пустое
+		showNoResults(); // вызов функции showNoResults
+	} else { // если поле не пустое
+		dataInput.setCustomValidity(''); // пустое поле, т.к. ошибки нет
+		e.preventDefault(); // у кнопки тип submit, отключаем стандартное поведение, которое приводит к перезагрузке страницы
+		tryFilterByType(typeInput.value.trim(), dataInput.value.trim()); // вызывов функции tryFilterByType, передаем туда тип данных и данные
 	}
 });
 
